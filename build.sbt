253f0c69dfe41fb7575dc6480d83d7a43058c86a
AlbanPhelip <alban.phelip@gmail.com>
2015-04-09 12:13:06 +0200

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "hands-on-mix-it-2015"
+
+version := "1.0"
+
+
+scalaVersion := "2.11.6"
+    
---
+++ src/main/scala/test/Test.scala
@@ -0,0 +1,13 @@
+package test
+
+/**
+ *
+ * Created by albanphelip on 09/04/15.
+ */
+object Test {
+
+  def main (args: Array[String]) {
+    println("test")
+  }
+
+}
