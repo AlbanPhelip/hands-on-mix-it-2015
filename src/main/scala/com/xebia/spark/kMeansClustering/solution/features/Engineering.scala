f3961b2acde24b357663b24af43cab26ef90ff87
AlbanPhelip <alban.phelip@gmail.com>
2015-04-09 15:13:48 +0200

    Working Kmeans

--- src/main/scala/com/xebia/spark/kMeansClustering/solution/features/Engineering.scala
+++ src/main/scala/com/xebia/spark/kMeansClustering/solution/features/Engineering.scala
@@ -1,7 +1,27 @@
 package com.xebia.spark.kMeansClustering.solution.features
 
+import org.apache.spark.mllib.linalg.{Vectors, Vector}
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.rdd.RDD
 
 
 object Engineering {
+
+  def featureEngineering(data : RDD[String]): RDD[(Vector, Double)] = {
+
+   data.map(line => {
+
+      val values = line.split('§')
+      val label = values(1).toDouble
+
+      val numericalData = Array(values(0), values(4), values(5), values(6), values(8)).map(l => l match {
+        case "NA" => 0d
+        case _ => l.toDouble
+      })
+
+     (Vectors.dense(numericalData), label)
+    })
+
+  }
 
 }
--- src/main/scala/com/xebia/spark/kMeansClustering/solution/kMeansClustering.scala
+++ src/main/scala/com/xebia/spark/kMeansClustering/solution/kMeansClustering.scala
@@ -1,21 +1,37 @@
 package com.xebia.spark.kMeansClustering.solution
 
+import com.xebia.spark.kMeansClustering.solution.features.Engineering.featureEngineering
 import com.xebia.spark.kMeansClustering.solution.tools.Utilities.extractHeader
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
+import org.apache.spark.mllib.clustering.KMeans
+import org.apache.spark.mllib.evaluation.MulticlassMetrics
 
 object kMeansClustering {
 
-
   def main(args: Array[String]) {
 
-    val conf = new SparkConf().setAppName("SMS_Spam_Classification").setMaster("local[4]").set("spark.executor.memory", "6g")
+    val conf = new SparkConf().setAppName("KMeans").setMaster("local[4]").set("spark.executor.memory", "6g")
     val sc = new SparkContext(conf)
 
     val rawData = sc.textFile("./src/main/resources/data_titanic.csv")
     val (header, data) = extractHeader(rawData)
 
-    println(header)
+    val cleanData = featureEngineering(data)
+
+    val Array(train, test) = cleanData.randomSplit(Array(0.6, 0.4))
+
+    val model = KMeans.train(train.map(_._1), 2, 10)
+
+    val prediction = model.predict(test.map(_._1))
+
+    val pred = test.map(l => (model.predict(l._1).toDouble, l._2))
+
+
+    val matrix = new MulticlassMetrics(pred)
+
+    println(matrix.confusionMatrix.toString())
+    println(prediction.sum())
 
 
   }
