dd327396be38ad45fc1681b62940505309134438
Yoann Benoit <ybenoit@xebia.fr>
2015-04-10 17:31:21 +0200

    Complete Random Forest with grid search

--- src/main/scala/com/xebia/spark/randomForestClassification/solution/RandomForestClassification.scala
+++ src/main/scala/com/xebia/spark/randomForestClassification/solution/RandomForestClassification.scala
@@ -3,7 +3,7 @@ package com.xebia.spark.randomForestClassification.solution
 
 import com.xebia.spark.randomForestClassification.solution.features.Engineering
 import com.xebia.spark.randomForestClassification.solution.tools.Utilities._
-import com.xebia.spark.randomForestClassification.solution.modelling.RandomForestObject.randomForestTrainClassifier
+import com.xebia.spark.randomForestClassification.solution.modelling.RandomForestObject.{randomForestTrainClassifier, gridSearchRandomForestClassifier}
 import org.apache.spark.{SparkContext, SparkConf}
 
 
@@ -24,12 +24,19 @@ object RandomForestClassification {
     val cleanedData = Engineering.featureEngineering(data)
 
     // Splitting data
-    val Array(trainSet, testSet) = cleanedData.randomSplit(Array(0.75, 0.25))
+    val Array(trainSet, valSet, testSet) = cleanedData.randomSplit(Array(0.7, 0.15, 0.15))
+
+    // Tuning parameters
+    val categoricalFeaturesInfo = Map(1 -> 2, 6 -> 4)
+    val bestParams = gridSearchRandomForestClassifier(trainSet, valSet,
+      categoricalFeaturesInfo = categoricalFeaturesInfo, numTreesGrid = Array(10, 20),
+      impurityGrid = Array("entropy", "gini"), maxDepthGrid = Array(5, 10), maxBinsGrid = Array(30, 50))
 
     // Modelling
-    val categoricalFeaturesInfo = Map(1 -> 2, 6 -> 4)
-    val model = randomForestTrainClassifier(categoricalFeaturesInfo = categoricalFeaturesInfo, numTrees = 50,
-      impurity = "entropy", maxDepth = 10, maxBins = 50)(trainSet)
+    val dataTrain = sc.union(trainSet, valSet)
+    val model = (randomForestTrainClassifier _).tupled(bestParams)(dataTrain)
+//    val model = randomForestTrainClassifier(categoricalFeaturesInfo = categoricalFeaturesInfo, numTrees = 50,
+//      impurity = "entropy", maxDepth = 10, maxBins = 50)(dataTrain)
 
     // Prediction & Evaluation
     val (accuracyTrain, confusionTrain) = getMetrics(model, trainSet)
